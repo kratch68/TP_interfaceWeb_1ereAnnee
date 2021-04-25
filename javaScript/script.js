96b63a38733900b21f7cf8536a13437bdb6cdc69
Gwenael Cabellic <cabellicg@gmail.com>
2021-04-25 11:08:12 -0400

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,39 @@
+function menu(x) 
+{
+  x.classList.toggle("change");
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function valider_rdv()
+
+{
+	if(document.getElementById('nom').value != "" && document.getElementById('courriel').value != "" && document.getElementById('date_selectionne').value != "")
+		{
+			document.getElementById("validation").style.visibility = "visible";
+		}
+}
